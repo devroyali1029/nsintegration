6de3acbd1662bb4ffda03705263a734201fb1c0f
devroyali1029 <dev.royali1029@gmail.com>
2017-10-11 02:34:46 +0200

    Update NSIntegration.php

--- NSIntegration.php
+++ NSIntegration.php
@@ -2,6 +2,7 @@
 	header('Content-Type: text/html');
 
 	$arg_list = func_get_args();
+echo "Arg List" . $arg_list;
     for ($i = 0; $i < $numargs; $i++) {
         echo "Argument $i is: " . $arg_list[$i] . "\n";
     }
